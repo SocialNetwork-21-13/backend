7e420904214de6ec73b1f766a4416e776e257e5c
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2022-10-06 00:22:58 +0300

    Update

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 import uvicorn
+from dotenv import dotenv_values
+from pymongo import MongoClient
 
+config = dotenv_values(".env")
 
 app = FastAPI(title="Ryglogram")
+
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)

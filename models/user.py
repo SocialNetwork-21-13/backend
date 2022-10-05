14067358502d8a0ffe6045a86598743fb3af8c8f
almostinf <almostinf@mail.ru>
2022-10-06 00:34:06 +0300

    added user in models

---
+++ models/user.py
@@ -0,0 +1,28 @@
+from datetime import date, datetime
+from typing import Optional
+from pydantic import BaseModel, EmailStr, validator, constr
+from pyparsing import Char
+
+class User(BaseModel):
+    id: Optional[str] = None
+    name: str
+    email: EmailStr
+    hashed_password: str
+    gender : Char
+    age : date
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+
+class UserIn(BaseModel):
+    name: str
+    email: EmailStr
+    password: constr(min_length=8)
+    password2: str
+    gender : Char
+    age : date
+
+    @validator("password2")
+    def password_match(cls, v, values, **kwargs):
+        if 'password' in values and v != values["password"]:
+            raise ValueError("passwords don't match")
+        return v

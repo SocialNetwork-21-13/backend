4b09bcc115d47c6ccefdaa133a167399c3a95e79
almostinf <almostinf@mail.ru>
2022-10-10 20:28:13 +0300

    added ability to work with files and create profile image

--- endpoints/user.py
+++ endpoints/user.py
@@ -1,29 +1,24 @@
-from fastapi import APIRouter, Body, Request, Response, HTTPException, status,Depends
-from fastapi.encoders import jsonable_encoder
+from fastapi import APIRouter, Body, Request, Response, HTTPException, status, File, UploadFile
 from typing import List
-
-from requests import request
-
-from repositories.user import get_all, create
+from repositories.user import get_all_users, create_user, get_file_id
 from models.user import User, UserIn
+
+# from pymongo import ReturnDocument
+# from fastapi.responses import JSONResponse, FileResponse
 
 router = APIRouter()
 
-''' 
-@router.post("/", response_description="Create a new user", status_code=status.HTTP_201_CREATED, response_model=UserIn)
-def create_user(request: Request, user: UserIn = Body(...)):
-    user = jsonable_encoder(user)
-    new_user = request.app.database["users"].insert_one(user)
-    created_user = request.app.database["users"].find_one(
-        {"_id": new_user.inserted_id}
-    )
-    return created_user
-'''
-
 @router.get("/", response_description="List all users", response_model=List[User])
 def list_users(request: Request, limit : int = 100, skip : int = 0):
-    return get_all(request.app.database, limit=limit, skip=skip)
+    return get_all_users(request.app.database, limit=limit, skip=skip)
 
-@router.post("/", response_description="Create a new user", status_code=status.HTTP_201_CREATED, response_model=UserIn)
-def create_user(request : Request, user: UserIn = Body(...)):
-    return create(request.app.database, user)
+@router.post("/", response_description="Create a new user", status_code=status.HTTP_201_CREATED, response_model=User)
+def create(request : Request, user: UserIn = Body(...)):
+    return create_user(request.app.database, user)
+
+@router.post("/{user_id}", response_description="Set a new profile image")
+def upload_file(request : Request, user_id : str, file: bytes = File()):
+    file_id = get_file_id(request.app.database, file)
+    request.app.database["users"].find_one_and_update({'_id': user_id},
+                        { '$set': { "profile_image" : file_id} },
+                        ) 
--- repositories/user.py
+++ repositories/user.py
@@ -1,21 +1,22 @@
-from datetime import datetime, date
+from datetime import datetime
 import datetime
 from typing import List, Optional
 from models.user import User, UserIn
 from core.security import hash_password
 from fastapi import HTTPException, status
 from fastapi.encoders import jsonable_encoder
+import gridfs
 
-def get_all(database, limit: int = 100, skip: int = 0) -> List[UserIn]:
+def get_all_users(database, limit: int = 100, skip: int = 0) -> List[User]:
     query = database["users"].find(limit=limit)
     return list(query)
 
-def get_by_id(database, id : int) -> Optional[UserIn]:
+def get_user_by_id(database, id : int) -> Optional[User]:
     if (user := database["users"].find_one({"_id": id})) is not None:
         return user
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with ID {id} not found")
 
-def create(database, u : UserIn) -> User:
+def create_user(database, u : UserIn) -> User:
     user = User (
         name=u.name,
         email=u.email,
@@ -31,9 +32,14 @@ def create(database, u : UserIn) -> User:
     created_user = database["users"].find_one(
         {"_id": new_user.inserted_id}
     )
-    return new_user
+    return created_user
 
 def get_by_email(database, email: str) -> User:
     if (user := database["users"].find_one({"email": email})) is not None:
         return user
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with email: {email} not found")
+
+def get_file_id(database, file : bytes) -> str:
+    imgs_profile = gridfs.GridFS(database, "imgs_profile")
+    obj = imgs_profile.put(file)
+    return str(imgs_profile.get(obj)._id)

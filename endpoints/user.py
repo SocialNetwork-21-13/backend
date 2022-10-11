77e16c04eeaf7494cab84af26515195820965e90
almostinf <almostinf@mail.ru>
2022-10-11 19:15:51 +0300

    added ability to create, update user and set, update profile image

--- endpoints/user.py
+++ endpoints/user.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Body, Request, Response, HTTPException, status, File, UploadFile
+from fastapi import APIRouter, Body, status, File
 from typing import List
-from repositories.user import get_all_users, create_user, get_file_id
+from repositories.user import UserRepository
 from models.user import User, UserIn
 
 # from pymongo import ReturnDocument
@@ -8,17 +8,45 @@ from models.user import User, UserIn
 
 router = APIRouter()
 
+users = UserRepository()
+
 @router.get("/", response_description="List all users", response_model=List[User])
-def list_users(request: Request, limit : int = 100, skip : int = 0):
-    return get_all_users(request.app.database, limit=limit, skip=skip)
+def list_users(limit : int = 100, skip : int = 0):
+    return users.get_all(limit=limit, skip=skip)
 
 @router.post("/", response_description="Create a new user", status_code=status.HTTP_201_CREATED, response_model=User)
-def create(request : Request, user: UserIn = Body(...)):
-    return create_user(request.app.database, user)
+def create(user: UserIn = Body(...)):
+    return users.create(user)
 
-@router.post("/{user_id}", response_description="Set a new profile image")
-def upload_file(request : Request, user_id : str, file: bytes = File()):
-    file_id = get_file_id(request.app.database, file)
-    request.app.database["users"].find_one_and_update({'_id': user_id},
-                        { '$set': { "profile_image" : file_id} },
-                        ) 
+@router.put("/{user_id}/profile_image", response_description="Set a new profile image")
+def upload_file(user_id : str, file: bytes = File()):
+    file_id = users.get_file_id(file)
+    return users.update_profile_image(user_id, file_id)
+
+@router.post("/upload_default", response_description="Upload default image", status_code=status.HTTP_201_CREATED)
+def upload_default_img(file : bytes = File()):
+    _ = users.get_file_id(file)
+
+@router.put("/{user_id}/name_surname", response_description="Update name and surname", response_model=User)
+def update_name_and_surname(user_id : str, name : str, surname : str):
+    return users.update_name_surname(user_id, name, surname)
+
+@router.put("/{user_id}/username", response_description="Update username", response_model=User)
+def update_user_username(user_id : str, username : str):
+    return users.update_username(user_id, username)
+
+
+'''
+@router.post("/upload", response_description="test", response_model=str)
+def test_upload(request : Request, file : bytes = File()):
+    test = gridfs.GridFS(request.app.database)
+    obj = test.put(file)
+    return str(test.get(obj)._id)
+
+@router.get("/upload", response_description="test")
+def test_get(request : Request, file_id : str):
+    test = gridfs.GridFS(request.app.database, "imgs_profile")
+    if test.exists(ObjectId(file_id)):
+        test.delete(ObjectId(file_id))
+    raise "Err"
+'''
--- repositories/user.py
+++ repositories/user.py
@@ -1,45 +1,75 @@
 from datetime import datetime
 import datetime
 from typing import List, Optional
+
+from bson import ObjectId
 from models.user import User, UserIn
 from core.security import hash_password
 from fastapi import HTTPException, status
 from fastapi.encoders import jsonable_encoder
 import gridfs
+from dotenv import dotenv_values
+from .base import BaseRepository
 
-def get_all_users(database, limit: int = 100, skip: int = 0) -> List[User]:
-    query = database["users"].find(limit=limit)
-    return list(query)
+config = dotenv_values(".env")
 
-def get_user_by_id(database, id : int) -> Optional[User]:
-    if (user := database["users"].find_one({"_id": id})) is not None:
-        return user
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with ID {id} not found")
+class UserRepository(BaseRepository):
+    def get_all(self, limit: int = 100, skip: int = 0) -> List[User]:
+        query = self.database["users"].find(limit=limit)
+        return list(query)
 
-def create_user(database, u : UserIn) -> User:
-    user = User (
-        name=u.name,
-        email=u.email,
-        hashed_password=hash_password(u.password),
-        username=u.username,
-        gender=u.gender,
-        age=u.age,
-        created_at=datetime.datetime.utcnow(),
-        updated_at=datetime.datetime.utcnow(),
-    )
-    user = jsonable_encoder(user)
-    new_user = database["users"].insert_one(user)
-    created_user = database["users"].find_one(
-        {"_id": new_user.inserted_id}
-    )
-    return created_user
+    def get_by_id(self, id : int) -> Optional[User]:
+        if (user := self.database["users"].find_one({"_id": id})) is not None:
+            return user
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with ID {id} not found")
 
-def get_by_email(database, email: str) -> User:
-    if (user := database["users"].find_one({"email": email})) is not None:
-        return user
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with email: {email} not found")
+    def create(self, u : UserIn) -> User:
+        user = User (
+            name=u.name,
+            surname=u.surname,
+            email=u.email,
+            hashed_password=hash_password(u.password),
+            username=u.username,
+            gender=u.gender,
+            age=u.age,
+            created_at=datetime.datetime.utcnow(),
+            updated_at=datetime.datetime.utcnow(),
+        )
+        user = jsonable_encoder(user)
+        new_user = self.database["users"].insert_one(user)
+        created_user = self.database["users"].find_one(
+            {"_id": new_user.inserted_id}
+        )
+        return created_user
 
-def get_file_id(database, file : bytes) -> str:
-    imgs_profile = gridfs.GridFS(database, "imgs_profile")
-    obj = imgs_profile.put(file)
-    return str(imgs_profile.get(obj)._id)
+    def get_by_email(self, email: str) -> User:
+        if (user := self.database["users"].find_one({"email": email})) is not None:
+            return user
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with email: {email} not found")
+
+    def get_file_id(self, file : bytes) -> str:
+        imgs_profile = gridfs.GridFS(self.database, "imgs_profile")
+        obj = imgs_profile.put(file)
+        return str(imgs_profile.get(obj)._id)
+
+    def update_name_surname(self, user_id : str, new_name : str, new_surname : str) -> User:
+        self.database["users"].find_one_and_update({'_id': user_id},
+                            { '$set': { "name" : new_name} },
+                            )
+        return self.database["users"].find_one_and_update({'_id': user_id},
+                            { '$set': { "surname" : new_surname} },
+                            )
+
+    def update_profile_image(self, user_id : str, file_id : str) -> User:
+        user = self.database["users"].find_one({"_id" : user_id})
+        if user["profile_image"] != str(config["DEFAULT_IMAGE_ID"]):
+            imgs_profile = gridfs.GridFS(self.database, "imgs_profile")
+            imgs_profile.delete(ObjectId(user["profile_image"]))
+        return self.database["users"].find_one_and_update({'_id': user_id},
+                            { '$set': { "profile_image" : file_id} },
+                            )
+
+    def update_username(self, user_id : str, new_username : str) -> User:
+        return self.database["users"].find_one_and_update({'_id': user_id},
+                            { '$set': { "username" : new_username} },
+                            )

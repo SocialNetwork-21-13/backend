135dd3d602bbd37a2121967931b310c3b005616f
almostinf <almostinf@mail.ru>
2022-10-21 22:13:24 +0300

    deleted all files

--- repositories/base.py
+++
@@ -1,9 +0,0 @@
-from dotenv import dotenv_values
-from pymongo import MongoClient
-
-config = dotenv_values(".env")
-
-class BaseRepository:
-    def __init__(self):
-        client = MongoClient(config["ATLAS_URI"])
-        self.database = client[config["DB_NAME"]]

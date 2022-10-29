7fb7ebbb3326bf4defb9ca347c7f58e28e7207e7
almostinf <almostinf@mail.ru>
2022-10-29 23:03:11 +0300

    added all needed modules to Pipfile and fixed codestyle in main

--- src/main.py
+++ src/main.py
@@ -10,15 +10,18 @@ config = dotenv_values(".env")
 
 app = FastAPI(title="Ryglogram")
 
+
 @app.on_event("startup")
 def startup_db_client():
     app.mongodb_client = MongoClient(config["ATLAS_URI"])
     app.database = app.mongodb_client[config["DB_NAME"]]
     print("Connected to the MongoDB database!")
 
+
 @app.on_event("shutdown")
 def shutdown_db_client():
     app.mongodb_client.close()
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, host="127.0.0.1", reload=True)
